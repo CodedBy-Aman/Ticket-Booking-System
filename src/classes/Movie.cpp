f64a898adbc21895118853d57f0da57ba0f89964
Amanpreet Singh <amansg421@gmail.com>
2026-09-14 09:34:09 +0530

    Add Movie.cpp

---
+++ src/classes/Movie.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Encapsulation: Movie owns its private data.
+class Movie {
+private:
+    string title, language;
+    int duration;
+
+public:
+    Movie(string title, string language, int duration)
+        : title(title), language(language), duration(duration) {}
+
+    string getTitle() const { return title; }
+    string getLanguage() const { return language; }
+    int getDuration() const { return duration; }
+
+    void display() const {
+        cout << title << " | " << language << " | " << duration << " min\n";
+    }
+};
